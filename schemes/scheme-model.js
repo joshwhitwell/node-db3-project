c44730759846f40c4b6320f829e971006b626ceb
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-04 16:43:08 -0500

    added find and findById helper methods

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,27 @@
 // scheme-model
+const db = require('../data/db-config')
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first()
+        .then(res => {
+            if (!res) {
+                return null
+            } else {
+                return res
+            }
+        })
+        .catch(err => {
+            return err
+        })
+}
+
+module.exports = {
+    find,
+    findById
+}
